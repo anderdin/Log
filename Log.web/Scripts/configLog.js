f509acb99ea887301d94aba3c3480f4339e320e1
Anderson Moreira <anderson.moreira@buildit.com.br>
2021-10-06 14:38:51 -0300

    Correções

---
+++ Log.web/Scripts/configLog.js
@@ -0,0 +1,17 @@
+﻿var host_api = 'https://localhost:44376/api/';
+
+function verificaSessao() {
+    if (localStorage.getItem("token") == null) {
+        window.location.replace('/Login');
+    } else {
+        $.post(host_api + 'Login/VerificaSessao?prToken=' + localStorage.getItem("token"), {}, function (res) {
+            if (res.Sucesso) {
+                if (!res.Dados.sessaoValida) {
+                    window.location.replace('/Login');
+                } else {
+                    window.location.replace('/Dashboard');
+                }
+            }
+        });
+    }
+}
